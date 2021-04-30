5d2cc57af5328200910ba6bb5d01b46425e74988
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2021-04-30 16:20:17 +0500

    change in icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  int count = 0;
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.lightGreen,
+      appBar: AppBar(
+        title: Text('Flutter world'),
+        backgroundColor: Colors.blueGrey[800],
+      ),
+      body: Center(
+        child: Image.asset(
+          'images/diamond.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 80,
+        ),
+        color: Colors.blue[900],
+        elevation: 50.0,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[900],
+        tooltip: 'Executable',
+        onPressed: () {
+          setState() => count++;
+        },
+      ),
+    ),
+  ));
+}
+
+/*void main() {
   var count;
   runApp(
     MaterialApp(
@@ -17,7 +53,11 @@ void main() {
           color: Colors.blueGrey[900],
         ),
         body: Center(
-          child: Text('Coding is easy..', textDirection: TextDirection.ltr),
+          child: Image(
+            image: NetworkImage(
+              'https://wallpapertag.com/wallpaper/full/0/b/2/974915-desktop-backgrounds-ocean-1920x1200-for-mac.jpg',
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
@@ -28,3 +68,4 @@ void main() {
     ),
   );
 }
+*/
